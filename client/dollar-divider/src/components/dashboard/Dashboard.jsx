401c002084a18e00e03fcfc3b070ef8adf1232b6
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-06-10 21:27:50 -0400

    removed test columns, added dashboard image

--- client/dollar-divider/src/components/dashboard/Dashboard.jsx
+++ client/dollar-divider/src/components/dashboard/Dashboard.jsx
@@ -1,31 +1,31 @@
 import { Col, Container, Row } from "reactstrap";
-
+// import dashboardImage from "../../../src/media/Dashboard_Layout.png"
+import dashboardImage from "../../media/Dashboard_Layout.png";
+import Bills from "./Bills";
 export default function Dashboard(props) {
-
   return (
     <>
+          {/* <Bills/> */}
       <Container>
         <Row>
-            <div>
-                Upcoming Bills
-            </div>
-        </Row>
-        <Row>
           <Col>
-            Test R1C1
-          </Col>
-          <Col>
-            Test R1C2
+          
           </Col>
         </Row>
         <Row>
-          <Col>
-            Test R2C1
-          </Col>
-          <Col>
-            Test R2C2
+          <Col
+          
+            // md="6"
+            style={{
+              width: "6em",
+            }}
+          >
+            
+            
+            <img src = {dashboardImage} alt="image" />
           </Col>
         </Row>
+
       </Container>
     </>
   );
