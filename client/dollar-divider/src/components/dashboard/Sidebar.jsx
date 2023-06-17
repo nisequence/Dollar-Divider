a0a052f60671b2ef3dc77d2d48ce47d63ba99fcd
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-06-17 16:01:16 -0400

    commented out link in sidebar

--- client/dollar-divider/src/components/dashboard/Sidebar.jsx
+++ client/dollar-divider/src/components/dashboard/Sidebar.jsx
@@ -1,22 +1,50 @@
-import React from "react";
-import { Progress } from "reactstrap";
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  // NavbarBrand,
+  Nav,
+  NavItem,
+  // NavLink,
+} from "reactstrap";
 
-export default function Sidebar() {
-  return <div id="sidebar" className="Sidebar">
-    <header>Sidebar lives here</header>
-    <button id="sidebarBtn" onClick={(e) => {
-      e.preventDefault()
-      const sidebar = document.getElementById("sidebar");
-      const sidebarBtn = document.getElementById("sidebarBtn");
-      sidebar.style.width = ".5em";
-      // sidebar.style.transition = width 2s;
-      sidebar.innerText = "";
-      sidebarBtn.style.width = ".5em";
-      sidebarBtn.style.marginLeft = ".5em";
+function Sidebar(props) {
+  const [collapsed, setCollapsed] = useState(true);
 
-      
-    }}></button>
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
+    const sidebar = document.getElementById("sidebar");
+    if (sidebar.style.width === "12em") {
+      sidebar.style.width = "0";
+    } else {
+      sidebar.style.height = "100vh";
+      sidebar.style.width = "12em";
+      sidebar.style.borderRight = "solid rgb(46, 46, 46) 2px";
+      sidebar.style.borderBottom = "solid rgb(46, 46, 46) 2px";
+      sidebar.style.borderTop = "solid rgb(144, 144, 144) 2px";
+      sidebar.style.borderLeft = "solid rgb(144, 144, 144) 2px";
+    }
+  };
 
-
-    </div>;
+  return (
+    <div className="Sidebar" id="sidebar">
+      <Navbar style={{ width: "10rem" }} color="faded" light>
+        <NavbarToggler
+          onClick={toggleNavbar}
+          className="me-2"
+          id="sidebarBtn"
+        />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              {/* <NavLink href="/components/">Components</NavLink> */}
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
+
+export default Sidebar;
