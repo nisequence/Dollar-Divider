71ef0b13717eda8636050eb601716798f23bdbf3
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-06-19 23:25:49 -0400

    started adding code for a toggle switch between hosehold and personal budget

--- client/dollar-divider/src/components/dashboard/sidebar/Sidebar.jsx
+++ client/dollar-divider/src/components/dashboard/sidebar/Sidebar.jsx
@@ -41,7 +41,43 @@ function Sidebar(props) {
   
     // }
     // }
-  let sidebarArray = ["Menu Item 1", "Menu Item 2", "Menu Item 3"];
+  let sidebarArray = ["Menu Item 1", "Menu Item 2", "Menu Item 3", 
+  // [
+  
+  //   import React, { useState } from 'react';
+  // import { Form, FormGroup, Label, Input } from 'reactstrap';
+  
+  // function Example(props) {
+  //   const [state, setState] = useState(true);
+  
+  //   return (
+  //     <Form>
+  //       <FormGroup switch>
+  //         <Input type="switch" role="switch" />
+  //         <Label check>Default switch checkbox input</Label>
+  //       </FormGroup>
+  //       <FormGroup switch>
+  //         <Input
+  //           type="switch"
+  //           checked={state}
+  //           onClick={() => {
+  //             setState(!state);
+  //           }}
+  //         />
+  //         <Label check>Checked switch checkbox input</Label>
+  //       </FormGroup>
+  //       <FormGroup switch disabled>
+  //         <Input type="switch" disabled />
+  //         <Label check>Disabled switch checkbox input</Label>
+  //       </FormGroup>
+  //       <FormGroup switch disabled>
+  //         <Input type="switch" checked disabled />
+  //         <Label check>Default switch checkbox input</Label>
+  //       </FormGroup>
+  //     </Form>
+  //   );
+  // }]
+];
   const sidebarItems = sidebarArray.map((i) => {
         return (
       <NavItem>{i}</NavItem>
