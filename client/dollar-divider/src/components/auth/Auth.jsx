d66807db7f0100d417ab1635b494b3c1137ac6d8
nisequence <nicolesekol@gmail.com>
2023-06-21 21:58:21 -0400

    added switch button for Register vs. Login form to appear - NS

--- client/dollar-divider/src/components/auth/Auth.jsx
+++ client/dollar-divider/src/components/auth/Auth.jsx
@@ -4,19 +4,60 @@ import Register from "./register/Register";
 import Login from "./login/Login";
 // Container is Reactstrap's resizing div w/ grid rows and columns
 // https://getbootstrap.com/docs/5.3/layout/grid/ (change class to className in jsx)
-import { Col, Container, Row } from "reactstrap";
+import { Col, Container, Row, Button } from "reactstrap";
+import { useState } from "react";
 
-  export default function Auth(props) {
+export default function Auth(props) {
+  //* useState to hold button's state login/sign-up
+  const [button, setButton] = useState("New user? Sign-up here!");
+
+  //* function to switch button from login to sign-up with setButton method
+  const swapForm = () => {
+    button === "Returning user? Login here!"
+      ? setButton("New user? Sign-up here!")
+      : setButton("Returning user? Login here!");
+  };
+
+  //* Ternary checks if button is set to "Login" message, if true user sees sign-up, if false user sees login
+  const displayForm = () => {
+    return button === "Returning user? Login here!" ? (
+      <Register updateToken={props.updateToken} />
+    ) : (
+      <Login updateToken={props.updateToken} />
+    );
+  };
+
   return (
     <>
       <Container>
         <Row>
+          <br></br>
+        </Row>
+        <Row>
           <Col md="6">
-            <Register updateToken = {props.updateToken}/>
+            <Button onClick={swapForm} color="dark">
+              {button}
+            </Button>
           </Col>
-          <Col md="6">
-            <Login updateToken = {props.updateToken}/>
+        </Row>
+        <br />
+        <Row>
+          <Col md="6">{displayForm()}</Col>
+          <Col>
+            <p>Insert Image Here</p>
           </Col>
+        </Row>
+        <Row>
+          <br></br>
+        </Row>
+        <Row>
+          <br></br>
+        </Row>
+        <Row>
+          <br></br>
+        </Row>
+        <Row>
+          <br></br>
         </Row>
       </Container>
     </>
