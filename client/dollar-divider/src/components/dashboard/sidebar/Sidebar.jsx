b741eccbbedca4214327f7981a5c181f6699b128
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-06-26 19:21:50 -0400

    mostly fixed the left sidebar issues. Still have a problem with the household and user buttons sometimes needing an extra click, but good enough for now

--- client/dollar-divider/src/components/dashboard/sidebar/Sidebar.jsx
+++ client/dollar-divider/src/components/dashboard/sidebar/Sidebar.jsx
@@ -13,6 +13,7 @@ import {
   // NavbarBrand,
   Nav,
   NavItem,
+  Button,
   // NavLink,
 } from "reactstrap";
 // import GetAll from "../accounts/getAll/GetAll";
@@ -31,10 +32,9 @@ function Sidebar(props) {
     const sidebar = document.getElementById("sidebar");
     const openSidebarWidth = "14vw";
     if (!collapsed) {
+      document.getElementById("sidebarItems").style.visibility = "hidden"
       sidebar.style.width = "0";
-
       sidebar.style.minWidth = "";
-      // sidebar.style.opacity = "1";
     } else {
       sidebar.style.height = "100vh";
       sidebar.style.width = openSidebarWidth;
@@ -43,22 +43,30 @@ function Sidebar(props) {
       sidebar.style.borderBottom = "solid rgb(46, 46, 46) 2px";
       sidebar.style.borderTop = "solid rgb(144, 144, 144) 2px";
       sidebar.style.borderLeft = "solid rgb(144, 144, 144) 2px";
+      setTimeout(() => {
+        document.getElementById("sidebarItems").style.visibility = "visible"  
+      }, 50);
+      
     }
   };
 
-  const [rightSideCollapsed, setRightSideCollapsed] = useState(true);
-  const toggleRightSidebar = () => {
-    setRightSideCollapsed(!rightSideCollapsed);
-    const rightSideMenu = document.getElementById("rightSideMenu");
+  const [userSettingsMenuCollapsed, setUserSettingsMenuCollapsed] = useState(true);
+  const toggleUserSettingsMenu = () => {
+    setUserSettingsMenuCollapsed(!userSettingsMenuCollapsed);
+    const userSettingsMenu = document.getElementById("userSettingsMenu");
     // const openRightSidebarWidth = "72vw";
     const openRightSidebarWidth = "95em";
-    if (!rightSideCollapsed) {
-      rightSideMenu.style.width = "0";
+    if (!userSettingsMenuCollapsed) {
+      userSettingsMenu.style.width = "0";
       // rightSideMenu.style.color = "rgba(0,0,0,0)";
       // rightSideMenu.style.minWidth = "fitContent";
     } else {
-      rightSideMenu.style.height = "100vh";
-      rightSideMenu.style.width = openRightSidebarWidth;
+      if (!householdSettingsMenuCollapsed){
+        document.getElementById("householdSettingsMenu").style.width="0";
+      }
+      setHouseholdSettingsMenuCollapsed(!householdSettingsMenuCollapsed);
+      userSettingsMenu.style.height = "100vh";
+      userSettingsMenu.style.width = openRightSidebarWidth;
       // rightSideMenu.style.maxWidth = "95em";
 
       // Get the current width of the left menu to base the max width of the right menu, to prevent overlap.
@@ -78,45 +86,92 @@ function Sidebar(props) {
       // rightSideMenu.style.maxWidth = `${maxRightWidth}vw`;
 
       // This should take into account the width of the left menu.
-      rightSideMenu.style.borderRight = "solid rgb(46, 46, 46) 2px";
-      rightSideMenu.style.borderBottom = "solid rgb(46, 46, 46) 2px";
-      rightSideMenu.style.borderTop = "solid rgb(144, 144, 144) 2px";
-      rightSideMenu.style.borderLeft = "solid rgb(144, 144, 144) 2px";
+      userSettingsMenu.style.borderRight = "solid rgb(46, 46, 46) 2px";
+      userSettingsMenu.style.borderBottom = "solid rgb(46, 46, 46) 2px";
+      userSettingsMenu.style.borderTop = "solid rgb(144, 144, 144) 2px";
+      userSettingsMenu.style.borderLeft = "solid rgb(144, 144, 144) 2px";
     }
   };
 
+  const [householdSettingsMenuCollapsed, setHouseholdSettingsMenuCollapsed] = useState(true);
+  const toggleHouseholdSettingsMenu = () => {
+    setHouseholdSettingsMenuCollapsed(!householdSettingsMenuCollapsed);
+    const householdSettingsMenu = document.getElementById("householdSettingsMenu");
+    // const openRightSidebarWidth = "72vw";
+    const openRightSidebarWidth = "95em";
+    if (!householdSettingsMenuCollapsed) {
+      householdSettingsMenu.style.width = "0";
+      // rightSideMenu.style.color = "rgba(0,0,0,0)";
+      // rightSideMenu.style.minWidth = "fitContent";
+    } else {
+      if (!userSettingsMenuCollapsed){
+        document.getElementById("userSettingsMenu").style.width="0";
+      }
+      setUserSettingsMenuCollapsed(!userSettingsMenuCollapsed);
+      householdSettingsMenu.style.height = "100vh";
+      householdSettingsMenu.style.width = openRightSidebarWidth;
+      // rightSideMenu.style.maxWidth = "95em";
+
+      // Get the current width of the left menu to base the max width of the right menu, to prevent overlap.
+      let temp = "";
+      let result = [];
+      let W = document.getElementById("sidebar").style.width;
+      for (let item of W) {
+        if (temp.length < 2) {
+          temp += item;
+          result.push(+temp);
+        }
+      }
+      // console.log(typeof result[1])
+      // let maxRightWidth = (80-result[1])+"vw";
+      // console.log(maxRightWidth)
+      // console.log(leftSideWidth, +leftSideWidth)
+      // rightSideMenu.style.maxWidth = `${maxRightWidth}vw`;
+
+      // This should take into account the width of the left menu.
+      householdSettingsMenu.style.borderRight = "solid rgb(46, 46, 46) 2px";
+      householdSettingsMenu.style.borderBottom = "solid rgb(46, 46, 46) 2px";
+      householdSettingsMenu.style.borderTop = "solid rgb(144, 144, 144) 2px";
+      householdSettingsMenu.style.borderLeft = "solid rgb(144, 144, 144) 2px";
+    }
+  };
+  // const toggleHouseholdSettingsMenu = () => {
+  //   console.log("HouseholdSettingsMenu Toggled")
+  // }
   //! ------------------------- Populate sidebarArray with Menu items -------------------
 
   //! -------------------------------- Navbar Links ----------------------------
 
+  let toggle = (
+    <Toggle setView={props.setView} view={props.view} />
+  )
+
   let householdSettings = (
-    <NavItem
-    // token={props.token}
-    // status={props.status}
-    // href="/dashboard/household"
-    >
-      <button onClick={toggleRightSidebar} id="householdSettings">
+    <NavItem>
+      <button onClick={toggleHouseholdSettingsMenu} id="householdSettingsButton">
         Household Settings
       </button>
     </NavItem>
   );
   let userSettings = (
-    <NavItem
-    // token={props.token}
-    // status={props.status}
-    // href="/dashboard/household"
-    >
-      <button onClick={toggleRightSidebar} id="userSettings">
+    <NavItem>
+      <button onClick={toggleUserSettingsMenu} id="userSettingsButton">
         User Settings
       </button>
     </NavItem>
   );
+
+  let logout = (
+    <Logout updateToken={props.updateToken} />
+  )
+
+  
   let br = <br></br>;
   let sidebarArray = [
-    "Menu Item 1",
-    "Menu Item 2",
+    toggle,
     userSettings,
     householdSettings,
+    logout
   ];
 
   const sidebarItems = sidebarArray.map((i) => {
@@ -125,31 +180,28 @@ function Sidebar(props) {
 
   return (
     <div className="Sidebar" id="sidebar">
-      <Navbar style={{ width: "10rem" }} color="faded" light>
-        <NavbarToggler
+      <div style={{ width: "10rem" }} color="faded" light>
+        <button
           onClick={toggleSidebar}
           className="me-2"
-          id="sidebarBtn"
-        />
-        <Collapse horizontal isOpen={!collapsed} navbar>
+          id="sidebarBtn"><i className="fa fa-bars"></i></button>
+        
+        <div>
+        {/* <Collapse horizontal isOpen={!collapsed} navbar> */}
           <Nav navbar>
             <br></br>
-            <NavItem>
-              <Toggle setView={props.setView} view={props.view} />
-            </NavItem>
             <br></br>
+            <div id="sidebarItems">
             {sidebarItems}
+            </div>
             <NavItem>{/* <EditBudgets /> */}</NavItem>
             <br></br>
-            <NavItem>
-              <Logout updateToken={props.updateToken} />
-            </NavItem>
             {/* //! Still working on implementation of both buttons working */}
             <HouseholdSettings token={props.token} />
             <UserSettings token={props.token} />
           </Nav>
-        </Collapse>
-      </Navbar>
+        </div>
+      </div>
     </div>
   );
 }
