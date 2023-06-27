755935c99849eb88edb559301734bd90b6d1b28b
nisequence <nicolesekol@gmail.com>
2023-06-26 22:01:23 -0400

    Member can now leave household - NS

---
+++ client/dollar-divider/src/components/dashboard/sidebar/householdSettings/views/member/functionality/Leave.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button } from "reactstrap";
+
+export default function Leave(props) {
+  async function quitHousehold(e) {
+    e.preventDefault();
+    const url = `http://localhost:4000/user/abandon`;
+
+    let requestOptions = {
+      headers: new Headers({
+        Authorization: props.token,
+        "Content-Type": "application/json",
+      }),
+      method: "PATCH",
+    };
+
+    try {
+      let response = await fetch(url, requestOptions);
+      let data = await response.json();
+
+      if (data.message == "User was successfully removed from the household!") {
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return (
+    <>
+      <Button color="danger" onClick={quitHousehold}>
+        Leave Household
+      </Button>
+    </>
+  );
+}
