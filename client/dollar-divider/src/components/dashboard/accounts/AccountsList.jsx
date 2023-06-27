90cc04ce9659b982b866e938b3d5b9ab1e508a14
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-06-26 22:48:20 -0400

    added accounts list fetch, still need to modify AccountsList.jsx with actual fetched data

--- client/dollar-divider/src/components/dashboard/accounts/AccountsList.jsx
+++ client/dollar-divider/src/components/dashboard/accounts/AccountsList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   CardGroup,
   Card,
@@ -8,42 +8,80 @@ import {
   CardSubtitle,
   Button,
 } from "reactstrap";
+export default function AccountsList(props) {
+  let name;
+  let balance;
+  let minBalance;
+  let allocations;
+  let available;
+  let ownerID;
+  let url;
+  name = "Fred's Bank"; //ToDO Replace w/fetched data
+  balance = 1000;
+  minBalance = 25;
+  allocations = [1, 2, 3, 4, 5];
+  available = 947;
+  ownerID = "6494e9996b0e90fc6f6489a9";
+  const [accounts, setAccounts] = useState([]);
+  const getAccounts = async (viewValue) => {
+    if (viewValue === true) {
+      url = "http://localhost:4000/finAccount/household";
+    } else {
+      url = "http://localhost:4000/finAccount/mine";
+    }
+    const reqOptions = {
+      method: "GET",
+      headers: new Headers({
+        Authorization: props.token,
+      }),
+    };
 
-export default function AccountsList() {
+    try {
+      const res = await fetch(url, reqOptions);
+      const data = await res.json();
+      console.log("Accounts Data:",data)
+      // If the server does not provide a failure message
+      if (data.message !== "No accounts found.") {
+        setAccounts(data.getAllUserFinAccounts);
+      } else {
+        setAccounts(null);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    
+  };
+
+  useEffect(() => {
+    if (props.token) {
+      getAccounts(props.view);
+    }
+  }, [props.token, props.view]);
+
+  //todo  Gonna have to change this to account for what's coming in through the fetch.
+let bank = [{"name": name, "balance": balance, "minBalance": minBalance, "allocations": allocations, "available": available, "ownerID": ownerID}]
+
+let finAcct = bank.map((item) => {
   return (
-    <>
-      <br></br>
-      <h4>My Accounts</h4>
-      <h6>Fake Data</h6>
-      <CardGroup>
-        <Card>
-          <CardBody>
-            <CardTitle tag="h5">TD Bank Savings</CardTitle>
-            <CardSubtitle className="mb-2 text-muted" tag="h6">
-              $400
-            </CardSubtitle>
-            <Button>Update</Button>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardBody>
-            <CardTitle tag="h5">Community Bank Checking</CardTitle>
-            <CardSubtitle className="mb-2 text-muted" tag="h6">
-              $750
-            </CardSubtitle>
-            <Button>Update</Button>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardBody>
-            <CardTitle tag="h5">Mattress Money</CardTitle>
-            <CardSubtitle className="mb-2 text-muted" tag="h6">
-              $900
-            </CardSubtitle>
-            <Button>Update</Button>
-          </CardBody>
-        </Card>
-      </CardGroup>
-    </>
-  );
+  <Card>
+    <CardBody>
+      <CardTitle tag="h5">{item.name}</CardTitle>
+      <CardSubtitle className="mb-2 text-muted" tag="h6">
+        ${item.balance}
+      </CardSubtitle>
+      {/* <Button>Update</Button> */}
+    </CardBody>
+  </Card>
+)
+})
+return (
+  <>
+   <CardGroup>
+  {finAcct}
+  {finAcct}
+  {finAcct}
+</CardGroup>
+<Button>Update</Button>
+</>
+)
 }
