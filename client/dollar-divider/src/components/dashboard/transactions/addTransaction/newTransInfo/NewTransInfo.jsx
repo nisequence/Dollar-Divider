300db05a4d41145f1ca78999e39a5a4f675753f0
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-07-06 14:03:34 -0400

    added datepicker, and switch for expense or income

--- client/dollar-divider/src/components/dashboard/transactions/addTransaction/newTransInfo/NewTransInfo.jsx
+++ client/dollar-divider/src/components/dashboard/transactions/addTransaction/newTransInfo/NewTransInfo.jsx
@@ -1,20 +1,66 @@
 import React, { useRef, useState } from "react";
-import { Form, FormGroup, Input, Button, Label } from "reactstrap";
+import { Form, FormGroup, Input, Button, Label, Col, Container, Row} from "reactstrap";
 import { format } from "date-fns";
 import { DayPicker } from "react-day-picker";
 // import DatePicker from "../../datePicker/DayPicker";
-import 'react-day-picker/dist/style.css';
+import "react-day-picker/dist/style.css";
+let transactionType;
 
 export default function NewTransInfo(props) {
+
   const [selected, setSelected] = React.useState();
-
-  let footer = <p>Please pick a day.</p>;
+  const [state, setState] = useState(true);
+  if (state === true) {
+    transactionType = "expense"
+  } else {transactionType = "income"}
+  console.log("transactionType",transactionType)
+  // ! Inspired By Kate
+  let month;
   if (selected) {
-    footer = <p>You picked {format(selected, 'PP')}.</p>;
+    month = selected.toString().slice(4, 7);
+    if (month === "Jan") {
+      month = "January";
+    }
+    if (month === "Feb") {
+      month = "February";
+    }
+    if (month === "Mar") {
+      month = "March";
+    }
+    if (month === "Apr") {
+      month = "April";
+    }
+    if (month === "Jun") {
+      month = "June";
+    }
+    if (month === "Jul") {
+      month = "July";
+    }
+    if (month === "Aug") {
+      month = "August";
+    }
+    if (month === "Sep") {
+      month = "September";
+    }
+    if (month === "Oct") {
+      month = "October";
+    }
+    if (month === "Nov") {
+      month = "November";
+    }
+    if (month === "Dec") {
+      month = "December";
+    }
+    sessionStorage.setItem("month:", month);
   }
-  console.log("selected",selected)
-  // const [selected, setSelected] = React.useState<Date>();
 
+  let day;
+  if (selected) {
+    day = parseInt(selected.toString().slice(8, 10));
+    // sessionStorage.setItem("day:", day);
+  }
+
+  // ! End Inspired By Kate
   const months = [
     "January",
     "February",
@@ -47,7 +93,7 @@ export default function NewTransInfo(props) {
   //const manualEntryRef= useRef();
   const finAccountRef = useRef();
   const categoryRef = useRef();
-  const typeRef = useRef();
+  // const typeRef = useRef();
 
   let base;
   if (props.view === false) {
@@ -60,31 +106,37 @@ export default function NewTransInfo(props) {
     e.preventDefault();
     const desc = descRef.current.value;
     const amount = amountRef.current.value;
-    let month = "February";
+    // let month = "February";
     // const month = monthRef.current.value; //Todo Change this to the Date Picker
-    let day = 2;
+    // let day = 2;
     // const day = dayRef.current.value; //Todo Change This to the Date Picker
     const category = categoryRef.current.value;
     const merchant = merchantRef.current.value;
-    const checkNumber = checkNumRef.current.value;
+    // const checkNumber = checkNumRef.current.value;
     //const manualEntry = manualEntryRef.current.value;
     // const finAccount = "fix this";
     const finAccount = finAccountRef.current.value;
-    const type = typeRef.current.value;
+    // const type = typeRef.current.value;
+    // const type = transactionType; 
 
     let url = "http://localhost:4000/transaction/add";
 
     let transObj = JSON.stringify({
       month: month,
-      day: day,
+      // month: JSON.stringify(sessionStorage.getItem("month")),
+      // day: day,
+      day: day = Number(day),
+      // month: "July",
       desc: desc,
       merchant: merchant,
       amount: amount,
-      checkNumber: checkNumber,
+      // checkNumber: checkNumber,
       //manualEntry: true,
       finAccount: finAccount,
+      // category: "category",
       category: category,
-      type: type,
+      // type: "expense",
+      type: transactionType,
       base: base,
     });
 
@@ -101,7 +153,6 @@ export default function NewTransInfo(props) {
     try {
       const res = await fetch(url, reqOption);
       const data = await res.json();
-
       // If the server provides a success message
       if (
         data.message === "You have created a new transaction!" ||
@@ -118,8 +169,8 @@ export default function NewTransInfo(props) {
   };
 
   return (
-    <>
-      <Form onSubmit={submitTrans}>
+    <div>
+      <Form id="addtransactionform" onSubmit={submitTrans}>
         {/* Name of Item */}
         <FormGroup>
           <Input
@@ -151,15 +202,20 @@ export default function NewTransInfo(props) {
           />
         </FormGroup>
         {/* Income or Expense */}
-        <FormGroup>
+        <div id="addTransSwitch">
+        <Label check>Expense</Label>
+        <FormGroup switch>
+          <Label check>Income</Label>
           <Input
-            placeholder="Income or Expense"
-            innerRef={typeRef}
-            autoComplete="off"
-            type="text"
-            required
+            type = "switch"
+            role="switch"
+            check={state}
+            onClick={() => {
+              setState(!state);
+            }}
           />
         </FormGroup>
+        </div>
         {/* Category */}
         <FormGroup>
           <Label for="exampleSelectMulti">Choose Category</Label>
@@ -179,53 +235,10 @@ export default function NewTransInfo(props) {
             })}
           </Input>
         </FormGroup>
-        {/* Date */}
+        {/* Day Picker */}
         <FormGroup>
-              <DayPicker 
-                    mode="single"
-                    selected={selected}
-                    onSelect={setSelected}
-                    footer={footer}
-              />
+          <DayPicker mode="single" selected={selected} onSelect={setSelected} />
         </FormGroup>
-        {/* Month */}
-        {/* <FormGroup>
-          <Label for="exampleSelectMulti">Choose Month</Label>
-          <Input
-            id="exampleSelect2"
-            name="select"
-            type="select"
-            innerRef={monthRef}
-            required
-          >
-            {months.map((each) => {
-              return (
-                <>
-                  <option value={each}>{each}</option>
-                </>
-              );
-            })}
-          </Input>
-        </FormGroup> */}
-        {/* Day */}
-        {/* <FormGroup>
-          <Label for="exampleSelect3">Choose Day</Label>
-          <Input
-            id="exampleSelect"
-            name="select"
-            type="select"
-            innerRef={dayRef}
-            required
-          >
-            {days.map((each) => {
-              return (
-                <>
-                  <option value={each}>{each}</option>
-                </>
-              );
-            })}
-          </Input>
-        </FormGroup> */}
         {/* Choose Account */}
         <FormGroup>
           <Label for="exampleSelectMulti">Choose Account</Label>
@@ -245,19 +258,10 @@ export default function NewTransInfo(props) {
             })}
           </Input>
         </FormGroup>
-        {/* Check Number */}
-        <FormGroup>
-          <Input
-            placeholder="Check Number"
-            innerRef={checkNumRef}
-            autoComplete="off"
-            type="number"
-          />
-        </FormGroup>
         <Button color="success" type="submit">
           Create Transaction
         </Button>
       </Form>
-    </>
+    </div>
   );
 }
