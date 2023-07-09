e34f8bd4f4589ad5370c7a12c385a68638f3fe17
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-07-09 11:46:53 -0400

    added get budgets fetch

--- client/dollar-divider/src/components/dashboard/budgets/Budgets.jsx
+++ client/dollar-divider/src/components/dashboard/budgets/Budgets.jsx
@@ -4,22 +4,8 @@ import { useState, useEffect } from "react";
 
 export default function Budgets(props) {
   let url;
-<<<<<<< HEAD
-  // const [budgets, setBudgets] = useState([]);
-
-  // const setHouseholdTotal = async (data) => {
-  //   let totalToDivide = 0;
-  //   for (let x = 0; x < data?.length; x++) {
-  //     let thisOne = data[x].budgetAmt;
-  //     totalToDivide += thisOne;
-  //   }
-  //   // console.log("View set to", props.view, "setting total to", totalToDivide);
-  //   sessionStorage.setItem("Total", totalToDivide);
-  // };
-=======
   const [budgets, setBudgets] = useState([]);
-  const [total, setTotal] = useState();
-
+  let [total, setTotal] = useState();
   const calculateTotal = async (data) => {
     let totalToDivide = 0;
     for (let x = 0; x < data?.length; x++) {
@@ -28,54 +14,37 @@ export default function Budgets(props) {
     }
     setTotal(totalToDivide);
   };
-
   const setHouseholdTotal = async (data) => {
     let totalToDivide = 0;
     for (let x = 0; x < data?.length; x++) {
       let thisOne = data[x].budgetAmt;
       totalToDivide += thisOne;
     }
-    // console.log("View set to", props.view, "setting total to", totalToDivide);
+
+    // if (totalToDivide) {
+    //   // console.log('totaltodivide',totalToDivide)
+    // }
     sessionStorage.setItem("Total", totalToDivide);
   };
->>>>>>> 093c00ce704c49001fa000c34ae6d076c30fa70e
 
-  // const getBudgets = async () => {
-  //   let viewValue = props.view;
-  //   if (viewValue === true) {
-  //     url = "http://localhost:4000/budget/household";
-  //   } else {
-  //     url = "http://localhost:4000/budget/mine";
-  //   }
-  //   const reqOptions = {
-  //     method: "GET",
-  //     headers: new Headers({
-  //       Authorization: props.token,
-  //     }),
-  //   };
+  const getBudgets = async () => {
+    let viewValue = props.view;
+    if (viewValue === true) {
+      url = "http://localhost:4000/budget/household";
+    } else {
+      url = "http://localhost:4000/budget/mine";
+    }
+    const reqOptions = {
+      method: "GET",
+      headers: new Headers({
+        Authorization: props.token,
+      }),
+    };
 
-  //   try {
-  //     const res = await fetch(url, reqOptions);
-  //     const data = await res.json();
+    try {
+      const res = await fetch(url, reqOptions);
+      const data = await res.json();
 
-<<<<<<< HEAD
-  //     // If the server does not provide a failure message
-  //     if (data.message === "Budget(s) found!") {
-  //       setBudgets(data.allBudgets);
-  //       if (
-  //         viewValue === true &&
-  //         url === "http://localhost:4000/budget/household"
-  //       ) {
-  //         setHouseholdTotal(data.allBudgets);
-  //       }
-  //     } else {
-  //       setBudgets(null);
-  //     }
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-=======
       // If the server does not provide a failure message
       if (data.message === "Budget(s) found!") {
         setBudgets(data.allBudgets);
@@ -93,11 +62,11 @@ export default function Budgets(props) {
       console.error(err);
     }
   };
->>>>>>> 093c00ce704c49001fa000c34ae6d076c30fa70e
 
   useEffect(() => {
     if (props.token) {
-      props.getBudgets();
+      getBudgets();
+      // props.getBudgets();
     }
   }, [props.token, props.view]);
 // console.log("props.transactions",props)
@@ -105,7 +74,7 @@ export default function Budgets(props) {
     <>
       <CurrentBudgetStatus
         getBudgets={props.getBudgets}
-        budgets={props.budgets}
+        budgets={budgets}
         transactions={props.transactions}
         transaction={props.transaction}
         getTransaction = {props.getTransaction}
