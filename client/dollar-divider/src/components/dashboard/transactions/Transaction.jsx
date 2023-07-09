105e72d6e8b0c9fa996aa189304d051e9072f4c3
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-07-09 11:54:24 -0400

    passed budgets, getaccounts, and getbudgets to RecentTransactions

--- client/dollar-divider/src/components/dashboard/transactions/Transaction.jsx
+++ client/dollar-divider/src/components/dashboard/transactions/Transaction.jsx
@@ -1,58 +1,5 @@
 import { useEffect, useState } from "react";
 import RecentTransactions from "./recentTransactions/RecentTransactions";
-<<<<<<< HEAD
-import React from "react";
-export default function Transaction(props) {
-  // let url;
-  // const [transaction, setTransaction] = useState([]);
-  
-  // const getTransaction = async () => {
-  //   let viewValue = props.view;
-  //   if (viewValue === true) {
-  //     url = "http://localhost:4000/transaction/household";
-  //   } else {
-  //     url = "http://localhost:4000/transaction/mine";
-  //   }
-  //   const reqOptions = {
-  //     method: "GET",
-  //     headers: new Headers({
-  //       Authorization: props.token,
-  //     }),
-  //   };
-  //   try {
-  //     const res = await fetch(url, reqOptions);
-  //     const data = await res.json();
-  //     // If the server does not provide a failure message
-  //     if (data.message !== "No transactions found.") {
-  //       setTransaction(data.getAllTransactions);
-  //     } else {
-  //       setTransaction(null);
-  //     }
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
-  // Get Transaction UseEffect
-  useEffect(() => {
-    if (props.token) {
-      props.getTransaction();
-    }
-  }, [props.token, props.view]);
-  // console.log("transactionprops",props)
-  return (
-    <>
-      <RecentTransactions
-        // transaction = {props.transaction} token={props.token} view={props.view} accounts = {props.accounts}             category = {props.category}
-        transaction={props.transaction}
-        token={props.token}
-        view={props.view}
-        accounts={props.accounts}
-        // category={props.category}
-        getTransaction={props.getTransaction}
-        getBudgets={props.getBudgets}
-        budgets={props.budgets}
-=======
 export default function Transaction(props) {
   let url;
   const [transaction, setTransaction] = useState([]);
@@ -102,7 +49,9 @@ export default function Transaction(props) {
         view={props.view}
         accounts={props.accounts}
         getTransaction={getTransaction}
->>>>>>> 093c00ce704c49001fa000c34ae6d076c30fa70e
+        budgets={props.budgets}
+        getAccounts = {props.getAccounts}
+        getBudgets = {props.getBudgets}
       />
     </>
   );
